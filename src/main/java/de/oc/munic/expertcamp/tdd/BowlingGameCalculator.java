dd4f3753baf65bd36bbddae33dfdae53fe33cedf
Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>
2019-09-20 15:03:50 +0200

    let your IDE generate the missing class and the method
    Signed-off-by: Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>

---
+++ src/main/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculator.java
@@ -0,0 +1,10 @@
+package de.oc.munic.expertcamp.tdd;
+
+public class BowlingGameCalculator {
+
+	public int validate(String rollsAllMiss) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
