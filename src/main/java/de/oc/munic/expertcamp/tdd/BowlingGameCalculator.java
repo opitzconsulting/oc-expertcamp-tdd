9804db589d1f9bc13264fe3317473972ddb77064
Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>
2019-09-20 16:07:42 +0200

    implement production code to meet both, first and second test.
    Signed-off-by: Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>

--- src/main/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculator.java
+++ src/main/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculator.java
@@ -1,10 +1,19 @@
 package de.oc.munic.expertcamp.tdd;
 
-public class BowlingGameCalculator {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-	public int validate(String rollsAllMiss) {
-		// TODO Auto-generated method stub
-		return 0;
+public class BowlingGameCalculator {
+	private static final int INITIAL_SCORE = 0;
+	private static final Pattern SINGLE_ROLL_PATTERN = Pattern.compile("(\\d)");
+
+	public int validate(String rolls) {
+		Matcher rollScore = SINGLE_ROLL_PATTERN.matcher(rolls);
+		int gameScore = INITIAL_SCORE;
+		while (rollScore.find()) {
+			gameScore += Integer.parseInt(rollScore.group());
+		}
+		return gameScore;
 	}
 
 }
