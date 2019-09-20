fc2f8b03fd847b732057488c3923bdf24d687891
Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>
2019-09-20 14:58:42 +0200

    First test generated by IDE (eclipse)
    Signed-off-by: Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>

---
+++ src/test/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculatorTest.java
@@ -0,0 +1,19 @@
+package de.oc.munic.expertcamp.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BowlingGameCalculatorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
