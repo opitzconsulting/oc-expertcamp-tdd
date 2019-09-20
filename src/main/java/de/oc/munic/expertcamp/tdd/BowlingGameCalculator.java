158dc0db5d9d2ba0bb4013e5c5baa0fb2eee80b7
Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>
2019-09-20 16:27:11 +0200

    refactoring: resolve "magic numbers"
    Signed-off-by: Thomas Papendieck <14850347+tpd-opitz@users.noreply.github.com>

--- src/main/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculator.java
+++ src/main/java/de/oc/munic/expertcamp/tdd/BowlingGameCalculator.java
@@ -5,10 +5,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BowlingGameCalculator {
+	private static final String FRAME_SEPARATOR = " ";
+	private static final int FRAME_SEPARATOR_MATCH = 2;
+	private static final int NEXT_ROLL_AFTER_PARE = 3;
+	private static final int FIRST_ROLL_IN_SPARE = 1;
+	private static final int ALL_PINS_SCORE = 10;
 	private static final int INITIAL_SCORE = 0;
+	private static final String NO_POINTS = "0";
 	private static final Pattern SINGLE_ROLL_PATTERN = Pattern.compile("(\\d)");
 	private static final Pattern SPARE_PATTERN = Pattern.compile("(\\d)/( )?(\\d)?");
 
+	
 	public int validate(String rolls) {
 		Matcher rollScore = SINGLE_ROLL_PATTERN.matcher(rolls);
 		int gameScore = INITIAL_SCORE;
@@ -18,9 +25,11 @@ public class BowlingGameCalculator {
 		Matcher spareScore = SPARE_PATTERN.matcher(rolls);
 		int restartPos = 0;
 		while (spareScore.find(restartPos)) {
-			gameScore += Integer.parseInt(Optional.ofNullable(spareScore.group(3)).orElse("0"))
-					+ (10 - Integer.parseInt(spareScore.group(1)));
-			restartPos = " ".equals(spareScore.group(2)) ? spareScore.start(2) : spareScore.end();
+			gameScore += Integer.parseInt(Optional.ofNullable(spareScore.group(NEXT_ROLL_AFTER_PARE)).orElse(NO_POINTS))
+					+ (ALL_PINS_SCORE - Integer.parseInt(spareScore.group(FIRST_ROLL_IN_SPARE)));
+			restartPos = FRAME_SEPARATOR.equals(spareScore.group(FRAME_SEPARATOR_MATCH))
+					? spareScore.start(FRAME_SEPARATOR_MATCH)
+					: spareScore.end();
 		}
 
 		return gameScore;
